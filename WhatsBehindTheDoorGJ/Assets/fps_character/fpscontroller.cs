d91b09a6c49814fd31fab3d1385eb23702aed3e9
lamadox <146740291+lamadox@users.noreply.github.com>
2024-02-11 15:27:31 +0300

    zenc
    i

--- WhatsBehindTheDoorGJ/Assets/fps_character/fpscontroller.cs
+++ WhatsBehindTheDoorGJ/Assets/fps_character/fpscontroller.cs
@@ -2,17 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class fpscontroller : MonoBehaviour
 {
+    public Camera playerCamera;
+    public float walkspeed = 6f;
+    public float runspeed = 12f;
+    public float jumppower = 7f;
+    public float gravity = 10f;
+
+    public float lookspeed = 2f;
+    public float lookxlimit = 45f;
+
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    public bool canMove = true;
+    CharacterController characterController;
+    
     // Start is called before the first frame update
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runspeed : walkspeed) * Input.GetAxis("vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runspeed : walkspeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+
+        //if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+     //   {
+      //      moveDirection.y = jumppower;
+      //  }
+       // else
+      //  {
+        //    moveDirection.y = movementDirectionY;
+     //   }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+
+      
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookspeed;
+            rotationX = Mathf.Clamp(rotationX, -lookxlimit, lookxlimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookspeed, 0);
+
+        }
+
     }
 }
