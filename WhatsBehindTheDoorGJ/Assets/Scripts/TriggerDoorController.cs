e87c1edee8233622d23134d70115dfa3281ad673
emiir2 <ahmetemiraydin20088@gmail.com>
2024-02-13 19:32:38 +0300

    geçiş yapıldı

--- WhatsBehindTheDoorGJ/Assets/Scripts/TriggerDoorController.cs
+++ WhatsBehindTheDoorGJ/Assets/Scripts/TriggerDoorController.cs
@@ -1,30 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerDoorController : MonoBehaviour
 {
     [SerializeField] private Animator myDoor = null;
     [SerializeField] private bool openTrigger = false;
-    
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime = 2.5f;
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && openTrigger)
         {
-            if (openTrigger)
-            {
-                myDoor.Play("DoorOpen", 0, 0.0f);
-                gameObject.SetActive(false);
-
-            }
-            
-
-
+            PlayDoorAnimation();
+            gameObject.SetActive(false);
+            Invoke(nameof(LoadNextLevel), transitionTime);
         }
     }
 
-
-
-
+    private void PlayDoorAnimation()
+    {
+        myDoor.Play("DoorOpen", 0, 0.0f);
+        transition.SetTrigger("Start");
+    }
 }
