f2a0fce2cf4cb12a6498459a1d8e50de82ac19b9
emiir2 <ahmetemiraydin20088@gmail.com>
2024-02-15 18:54:03 +0300

    quest system çalışmalarına başlandı

---
+++ WhatsBehindTheDoorGJ/Assets/Scripts/IEnemy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemy
+{
+    int ID { get; set;  }
+    Spawner Spawner { get; set; }
+    int Experience { get; set; }
+    void Die();
+    void TakeDamage(int amount);
+
+    void PerformAttack();
+
+
+
+    
+}
---
+++ WhatsBehindTheDoorGJ/Assets/Scripts/Sword_Enemies.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Sword_Enemies : MonoBehaviour
+{
+    public Spawner spawner { get; set; }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
