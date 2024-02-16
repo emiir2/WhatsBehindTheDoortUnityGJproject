260ec82c6d3a8e250b87784c916a3010b19530ee
emiir2 <ahmetemiraydin20088@gmail.com>
2024-02-16 23:32:33 +0300

    health bar

--- WhatsBehindTheDoorGJ/Assets/Scripts/fff.cs
+++ WhatsBehindTheDoorGJ/Assets/Scripts/fff.cs
@@ -13,6 +13,9 @@ public class SC_FPSController : MonoBehaviour
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 60.0f;
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
@@ -23,6 +26,8 @@ public class SC_FPSController : MonoBehaviour
 
     void Start()
     {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
 
         characterController = GetComponent<CharacterController>();
 
@@ -70,5 +75,11 @@ public class SC_FPSController : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+    }
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+
     }
 }
